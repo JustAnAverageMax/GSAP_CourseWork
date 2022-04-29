f863dc54c46c85f24c3b27ece4d920bc4b6336c7
bobothesexybear <80481012+bobothesexybear@users.noreply.github.com>
2022-04-29 16:02:36 +0300

    Added damagable state to player

--- Assets/DetectTrigger.cs
+++ Assets/DetectTrigger.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.FPS.Game;
+using Unity.FPS.Gameplay;
 using UnityEngine;
 
 public class DetectTrigger : MonoBehaviour
@@ -8,6 +10,11 @@ public class DetectTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        Damageable damageable = other.GetComponent<Damageable>();
+        if (damageable)
+        {
+            damageable.InflictDamage(transform.parent.GetComponent<Fireball>().damage, false, transform.parent.gameObject);
+        }
         Destroy(transform.parent.gameObject);
     }
 }
